6f6cf7289f97e1cd7fa6d333c7c72b15b2b8d79b
pviwatkurkul <pviwatkurkul@ucsd.edu>
2022-10-06 12:56:27 -0700

    search engine

--- NumberServer.java
+++ NumberServer.java
@@ -5,7 +5,7 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-
+    
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
             return String.format("Number: %d", num);
