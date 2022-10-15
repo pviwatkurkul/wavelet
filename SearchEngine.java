7bc2780a28e976c5c7a6968622d8ce8cb89c4fa6
pviwatkurkul <pviwatkurkul@ucsd.edu>
2022-10-14 21:32:02 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -3,7 +3,6 @@ import java.net.URI;
 import java.util.ArrayList;
 
 
-    /* 
     class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
@@ -31,42 +30,6 @@ import java.util.ArrayList;
                     String[] parameters = url.getQuery().split("=");
                     if (parameters[0].equals("s")) {
                         word.add(parameters[1]);
-                        return String.format("String added");
-                    }
-                }
-                return "404 Not Found!";
-            }
-    }
-}
-*/
-
-class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-        ArrayList<String> enterWord = new ArrayList<String>();
-        String Wordsearched = new String();
-        String subString = "";
-        public String handleRequest(URI url) {
-            if (url.getPath().equals("/")) {
-                return String.format("String not found");
-            } else if (url.getPath().equals("/seek")) {
-                String[] parameters1 = url.getQuery().split("=");
-                    if (parameters1[0].equals("start")) {
-                        subString = parameters1[1];
-                    for (int i = 0; i< enterWord.size(); i++){
-                            if (enterWord.get(i).contains(subString)){
-                                Wordsearched += (enterWord.get(i) + " ");
-                        }
-                    }
-                    return String.format(Wordsearched);
-                }
-                return "404 Not Found";
-            } else {
-                System.out.println("Path: " + url.getPath());
-                if (url.getPath().contains("/add")) {
-                    String[] parameters = url.getQuery().split("=");
-                    if (parameters[0].equals("start")) {
-                        enterWord.add(parameters[1]);
                         return String.format("String added");
                     }
                 }
